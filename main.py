4b7f41d2e419b993ceed2c477abd7ce9e7fa6986
aakash013srivastava <aakash013srivastava@gmail.com>
2022-11-18 17:37:53 +0530

    Messages to respective emails in latest excel downloaded is automated

--- main.py
+++ main.py
@@ -12,33 +12,35 @@ global count
 
 root = Tk()
 root.title('Send Email to excel file recipients')
-root.geometry('400x200')
-root.minsize(400,200)
-root.maxsize(400,200)
+root.geometry('500x200')
+root.minsize(500,200)
+root.maxsize(500,200)
 
-def send_mails(receivers,mailBody):
+def send_mails(receivers,messages):
+    print("in sendMails")
     email_sender = 'aakashsrivastava013@gmail.com' # Put registered email with 2 factor authentication here
-    email_receiver = receivers
     email_password = 'aiedmjzfmbmvsigm' # Put your app password here
 
+    for i in range(0,len(receivers)):
 
-    subject = 'TestEmail'
-    body = mailBody
+        email_receiver = receivers[i]
+        subject = 'ForwardedEmail'
+        body = messages[i]
 
-    em = EmailMessage()
-    em['From'] = email_sender
-    em['To'] = email_receiver
-    em['Subject'] = subject
-    em.set_content(body)
+        em = EmailMessage()
+        em['From'] = email_sender
+        em['To'] = email_receiver
+        em['Subject'] = subject
+        em.set_content(body)
 
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
-        smtp.login(email_sender,email_password)
-        smtp.sendmail(email_sender,email_receiver,em.as_string())
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
+            smtp.login(email_sender,email_password)
+            smtp.sendmail(email_sender,email_receiver,em.as_string())
 
 # -------------------------------------------------------------------------
 #------------------------------------------------------------------------------
-def get_latest_excel_file(mailBody):
+def get_latest_excel_file():
     username = str(os.getlogin())
     path1 = '/home/'+username+'/Downloads/*.xlsx'
     list_of_files = glob.glob(path1) #Get all excel python files with path
@@ -67,30 +69,34 @@ def get_latest_excel_file(mailBody):
         f.close()
     if not flag:  
         try:
-            email_list = list(pd.read_excel(latest_file)['Email'])
-            ls = [x for x in email_list if type(x) == str]
-            send_mails(ls,mailBody)
-            count = len(ls)
-            msg = messagebox.showinfo("Alert",str(count)+" mails sent").pack()
-        except Exception as e:
-            print(e)
-        else:
+            recipient_list = list(pd.read_excel(latest_file)['Email'])
+            message_text_list = list(pd.read_excel(latest_file)['Message'])
+            send_mails(recipient_list,message_text_list)
+            message_alert = str(pd.read_excel(latest_file)['Message'].count())+" mails sent"
+            msg = messagebox.showinfo("Alert",message_alert)
+            msg.pack()
             f = open('db.txt','a+')
             
-            f.write(latest_file_name+":"+str(count)+" Mails Sent"+'\n')
+            f.write(latest_file_name+":"+str(pd.read_excel(latest_file)['Message'].count())+" Mails Sent"+'\n')
             f.close()
+        except Exception as e:
+            print((e))
+        
+            
     else:
-        msg = messagebox.showinfo("Alert","Mails already sent to latest file").pack()
+        print("executed")
+        msg = messagebox.showinfo("AlertAlreadySent","Mails already sent to latest file").pack()
     
 # -----------------------------------------------------------------------------
 
 
 
 
-newFileSelectLabel = Label(text='Latest file in directory:').pack()
-mailBodyText = Label(text='Enter mail message:').pack()
-mailBody = Entry(root).pack()
-checkNewFileButton = Button(text='Send',command=get_latest_excel_file(mailBody)).pack()
+newFileSelectLabel = Label(text='Email messages to addresses from latest excel file in downloads folder').pack()
+# mailBodyText = Label(text='Enter mail message:').pack()
+# mailBody = Text(root,height=5,width=20).pack()
+checkNewFileButton = Button(text='Send',command=get_latest_excel_file).pack()
+
 
 root.mainloop() 
 
